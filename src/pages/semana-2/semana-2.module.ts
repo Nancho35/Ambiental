50121e44f3ff48efd20d9e399d88daa67ff50bba
Hernan <hernandario35@gmail.com>
2018-09-30 18:29:57 -0500

    version 0.00001

---
+++ src/pages/semana-2/semana-2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Semana_2Page } from './semana-2';
+
+@NgModule({
+  declarations: [
+    Semana_2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Semana_2Page),
+  ],
+})
+export class Semana_2PageModule {}
